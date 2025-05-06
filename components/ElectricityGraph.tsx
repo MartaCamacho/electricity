04ca6b44d1dd65a686034bb03762b18c6b208ea5
martacamacho <martacamachovargas@gmail.com>
2025-05-06 22:53:47 +0200

    remove unused code from default template

---
+++ components/ElectricityGraph.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ElectricityGraph = () => {
+  return (
+    <View>
+      <Text>ElectricityGraph</Text>
+    </View>
+  )
+}
+
+export default ElectricityGraph
+
+const styles = StyleSheet.create({})
